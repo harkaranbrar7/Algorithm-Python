00e3883cf47a75d47a2efc7b2c853a0cba64aac4
Harkaran Brar <harkaranbrar7@gmail.com>
2020-01-08 10:13:16 -0800

    nimGame

---
+++ nimGame.py
@@ -0,0 +1,11 @@
+class Solution:
+    def canWinNim(self, n: int) -> bool:
+        return 
+        if n % 4 == 0:
+            return False
+        else:
+            return True
+
+
+test1 = Solution()
+print(test1.canWinNim(4))
