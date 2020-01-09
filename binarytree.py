d6cd7caea7e6eb9d07dc9c8cbcd6f0b316c58de4
Harkaran Brar <harkaranbrar7@gmail.com>
2020-01-09 15:41:37 -0800

    BinaryTree

--- binarytree.py
+++ binarytree.py
@@ -1,17 +1,20 @@
-def solution (arr):
-    if not arr: return ""
-    if len(arr) == 0: return ""; 
+def solution(arr):
+    if not arr:
+        return ""
+    if len(arr) == 0:
+        return ""
 
-    def sum (arr, idx):
-        if idx - 1 < len(arr): 
-            if arr[idx - 1] == -1: return 0
+    def sum(arr, idx):
+        if idx - 1 < len(arr):
+            if arr[idx - 1] == -1:
+                return 0
             return arr[idx - 1] + sum(arr, idx * 2) + sum(arr, idx * 2 + 1)
-        
+
         return 0
 
     left = sum(arr, 2)
     right = sum(arr, 3)
-    
+
     if left == right:
         return ""
     if left > right:
@@ -20,5 +23,5 @@ def solution (arr):
         return "Right"
 
 
-print(solution([3,6,2,9,-1,10]))
-#output Left
+print(solution([3, 6, 2, 9, -1, 10]))
+# output Left
