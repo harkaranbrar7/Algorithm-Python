b4a536f4598fcf3dc2c5ea9c8699bf83cba2f1c5
Harkaran Brar <harkaranbrar7@gmail.com>
2020-01-14 20:47:13 -0800

    InsertionSort

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,18 +1,24 @@
-def bubbleSort(arr):
-    n = len(arr)
 
-    for i in range(n):
-        for j in range(0,n-i-1):
-            if arr[j] >arr[j+1]:
-                arr[j],arr[j+1]= arr[j+1],arr[j]
+class Solution:
+
+    def __init__(self, arr):
+        self.arr = arr
+
+    def bubbleSort(self):
+        n = len(self.arr)
+
+        for i in range(n):
+            for j in range(0,n-i-1):
+                if self.arr[j] >self.arr[j+1]:
+                    self.arr[j],self.arr[j+1]= self.arr[j+1],self.arr[j]
 
 
-def insertionSort(arr):
-    for i in range(1,len(arr)):
-        key = arr[i]
+    def insertionSort(self):
+        for i in range(1,len(self.arr)):
+            key = self.arr[i]
 
-        j = i-j
-        while j>=0 and key <arr[j]:
-            arr[j+1]=arr[j]
-            j-=1
-        arr[j+1]=key
+            j = i-j
+            while j>=0 and key <self.arr[j]:
+                self.arr[j+1]=self.arr[j]
+                j-=1
+            self.arr[j+1]=key
