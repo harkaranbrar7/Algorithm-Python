65b6dd3b35aacfbb1fb0353527620409a40c3164
Harkaran Brar <harkaranbrar7@gmail.com>
2020-01-17 18:42:36 -0800

    alphaSort

---
+++ alphaSort.py
@@ -0,0 +1,17 @@
+
+
+class Solution:
+
+    def __init__(self,str):
+        self.str = str
+
+    def sort(self):
+        words = self.str.split()
+        words.sort()
+        return ' '.join((i) for i in words)
+
+
+test1 = Solution(input("Enter a String: "))
+
+print("The sorted words are: ", test1.sort())
+
