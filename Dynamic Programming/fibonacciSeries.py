e9e58bccc489964d1ef4f93e3c88d07e85da4f54
Harkaran-Brar <harkarnbrar7@gmail.com>
2020-01-27 12:19:00 -0800

    initial

--- Dynamic Programming/fibonacciSeries.py
+++ Dynamic Programming/fibonacciSeries.py
@@ -1,16 +1,21 @@
 # Fibonacci Series using Dynamic Programming  
-def fibonacci(n):  
+def fibonacci(n): 
+    # check for n is zero or less than two 
+    
     if n == 0:
         return 0
     elif n <= 2:
         return 1
 
+    # List of zeros with length of given value n
     seq = [0] * n
+    # Fibonacci numbers are start with 1,1
     seq[0] = seq[1] = 1
 
+    # Algorithm Calculating Fibonacci Numbers
     for i in range(2,n):
         seq[i]= seq[i-1]+seq[i-2]
     
     return seq[n-1]
 
-print(fibonacci(10))
+print(fibonacci(9))
